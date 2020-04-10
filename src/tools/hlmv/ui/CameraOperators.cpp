83128c9ebc4e3c981d9214b909f878b00bd3a50c
Sam <duo929@hotmail.com>
2020-04-10 22:48:50 +0200

    Minor formatting fix

--- src/tools/hlmv/ui/CameraOperators.cpp
+++ src/tools/hlmv/ui/CameraOperators.cpp
@@ -141,7 +141,7 @@ void FirstPersonCameraOperator::UpdateCamera(entt::registry& registry, entt::ent
 	auto& rotationEulerXYZ = registry.get<game::components::RotationEulerXYZ>(cameraEntity);
 
 	//Set view to what the game uses for first person
-	translation.Value= graphics::components::FirstPersonPosition;
+	translation.Value = graphics::components::FirstPersonPosition;
 
 	rotationEulerXYZ.Value = glm::vec3{0.f};
 }
